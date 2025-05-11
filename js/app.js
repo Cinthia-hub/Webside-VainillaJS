48fdbf22441c1ba4706b7f631f7045ef8f20cedb
Cinthia-hub <ccamibrm@gmail.com>
2025-05-10 19:54:17 -0600

    [ADDED]: Video 10

---
+++ js/app.js
@@ -0,0 +1,105 @@
+console.log('Conectado')
+
+const wrapper = document.querySelector(".sliderWrapper")
+const menuItems = document.querySelectorAll("menuItem")
+
+const product = [
+    {
+        id: 1,
+        title: "Air Force",
+        price: 119,
+        colors: [ 
+            {
+                code: "black",
+                img: "./img/air.png"
+            },
+            {
+                code: "darkblue",
+                img: "./img/air2.png"
+            }
+        ] 
+    },
+    {
+        id: 2,
+        title: "Jordan",
+        price: 149,
+        colors: [ 
+            {
+                code: "lightgray",
+                img: "./img/jordan.png"
+            },
+            {
+                code: "green",
+                img: "./img/jordan2.png"
+            }
+        ] 
+    },
+    {
+        id: 3,
+        title: "Air Blazer",
+        price: 109,
+        colors: [ 
+            {
+                code: "lightgray",
+                img: "./img/blazer.png"
+            },
+            {
+                code: "darkblue",
+                img: "./img/blazer2.png"
+            }
+        ] 
+    },
+    {
+        id: 4,
+        title: "Crater",
+        price: 129,
+        colors: [ 
+            {
+                code: "black",
+                img: "./img/crater.png"
+            },
+            {
+                code: "lightgray",
+                img: "./img/crater2.png"
+            }
+        ] 
+    },
+    {
+        id: 5,
+        title: "Hippie",
+        price: 99,
+        colors: [ 
+            {
+                code: "gray",
+                img: "./img/hippie.png"
+            },
+            {
+                code: "darkblue",
+                img: "./img/hippie2.png"
+            }
+        ] 
+    }
+]
+
+let choosenProduct = products[0]
+
+const currentProductImg = document.querySelector(".productImg")
+const currentProductTitle = document.querySelector(".productTitle")
+const currentProductPrice = document.querySelector(".productPrice")
+const currentProductColors = document.querySelector(".color")
+const currentProductSizes = document.querySelector(".size")
+
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper.computedStyleMap.transform = `translateX(${-100 * index}vw)`
+        choosenProduct = product[index]
+        currentProductTitle.textContent = choosenProduct.title
+        currentProductPrice.textContent = choosenProduct.price
+        currentProductImg.src = choosenProduct.colors[0].img
+
+        currentProductColors.forEach((color, index) => {
+            color.style.backgroundColor = choosenProduct.colors[index].code
+        })
+        console.log(choosenProduct)
+    })
+})
